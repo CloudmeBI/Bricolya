3a24bf9aac7471c99335252d890143544ceb49f0
Reda <redabelca96@gmail.com>
2020-11-25 09:41:19 +0100

    payment methods in footer

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -1,71 +1,200 @@
 <footer class="row">
-        <div class="row m0 topFooter">
+    <div class="row m0 topFooter">
 
-            <div class="container line2">
-                <div class="row">
-                    <div class="col-sm-3 widget">
-                        <div class="row m0">
-                          <div class="media">
-                              <img class="mb15" src="https://bricolya.com/public/frontend/images/bricolya-logo-white.png" alt="Bricolya logo">
-                          </div>
-                            <h4>Suivez-nous</h4>
-                            <ul class="list-inline">
-                                <li><a href="https://www.facebook.com/Bricolya-106099687864605/?modal=admin_todo_tour" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="https://www.instagram.com/bricolya/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-                                <li><a href="https://www.youtube.com/channel/UCME4qaKamEt7kt7JWiXa5rA/?guided_help_flow=5" target="_blank"><i class="fa fa-youtube"></i></a></li>
-                                <li><a href="https://www.linkedin.com/company/66952647/admin/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-
-                            </ul>
+        <div class="container line2">
+            <div class="row">
+                <div class="col-sm-3 widget">
+                    <div class="row m0">
+                        <div class="media">
+                            <img class="mb15" src="https://bricolya.com/public/frontend/images/bricolya-logo-white.png" alt="Bricolya logo">
                         </div>
-                    </div>
-                    <div class="col-sm-1 col-xs-2">
-                        <!-- <div class="col-sm-6 m0"> -->
+                        <h4>Suivez-nous</h4>
+                        <ul class="list-inline">
+                            <li><a href="https://www.facebook.com/Bricolya-106099687864605/?modal=admin_todo_tour" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://www.instagram.com/bricolya/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="https://www.youtube.com/channel/UCME4qaKamEt7kt7JWiXa5rA/?guided_help_flow=5" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                            <li><a href="https://www.linkedin.com/company/66952647/admin/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
 
-
-                    </div>
-                    <div class="col-sm-4 widget">
-                        <!-- <div class="col-sm-6 m0"> -->
-                        <h4>Plan du site</h4>
-                        <ul class="nav col-xs-6">
-                              <li><a href="{{url('/')}}">Accueil</a></li>
-                              <li><a href="{{url('aboutus')}}">À propos</a></li>
-                              <li><a href="{{url('all_products')}}">Nos produits</a></li>
-                              <li><a href="{{url('flash_products')}}">Promotions</a></li>
                         </ul>
-                            <ul class="nav col-xs-6">
-                                <li><a href="{{url('flash_products')}}">Destockage</a></li>
-                                <li><a href="{{url('recruitment')}}">Recrutement</a></li>
-                                <li><a href="{{url('videos')}}">Vidéothèque</a></li>
-                                <li><a href="{{url('contact')}}">Contact</a></li>
-                            </ul>
-
                     </div>
+                    <style>
+                        .nss .fi-er~.dp:not(:focus),
+                        .nss .fi-er~.fi:not(:focus),
+                        .nss .fi-er~.sel:not(:focus) {
+                            border-bottom: 2px solid #b81a0a
+                        }
 
-                    <div class="col-sm-4 widget">
-                        <div class="text-center mb30">
-                            <h4>Contact</h4>
-                            <p>Adresse: High dis 99-100 rue Metz Casablanca - Maroc</p>
-                            <p>Téléphone: +212 522 450 854</p>
-                            <p>Email: contact@bricolya.com </p>
+                        .nss .fi-er+.ta:not(:focus) {
+                            outline: 1px solid #b81a0a;
+                            border-color: #b81a0a
+                        }
+
+                        .nss .var-w .vi:not(:disabled):not(:checked)+.vl:hover {
+                            border: 1px solid #c7c7cd
+                        }
+
+                        .nss .ts-w._dis input:not(:checked)~.lbl+* .ts-i {
+                            color: #c7c7cd;
+                            pointer-events: none
+                        }
+
+                        .nss .ic {
+                            fill: #282828
+                        }
+
+                        .nss a {
+                            background-color: transparent;
+                            display: inline-block;
+                            text-decoration: none;
+                            color: inherit;
+                            -webkit-text-decoration-skip: objects
+                        }
+
+                        .nss .dpdw .tgl:not(:checked)+.trig:hover,
+                        .nss .dpdw._hov:hover .tgl:not(:checked)+.trig {
+                            color: #f68b1e
+                        }
+
+                        .nss .dpdw .tgl:not(:checked)+.trig:hover svg,
+                        .nss .dpdw._hov:hover .tgl:not(:checked)+.trig>svg {
+                            fill: #f68b1e
+                        }
+
+                        .nss .dpdw._hov:hover .tgl:not(:checked)~.box {
+                            display: block
+                        }
+
+                        .nss .cms .tabs>[type=radio]:not(:checked)+label+div,
+                        .nss .dpdw .tgl:not(:checked)~.box,
+                        .nss .ts-w._dn input:not(:checked)~.ts-i {
+                            display: none
+                        }
+
+                        .nss .row {
+                            display: flex
+                        }
+
+                        .nss .row {
+                            flex: 0 1 auto;
+                            flex-wrap: wrap;
+                            width: 100%;
+                            max-width: 1184px
+                        }
+
+                        .nss .col12,
+                        .nss .col16,
+                        .nss .row[class*=col] {
+                            padding-right: 8px;
+                            padding-left: 8px
+                        }
+
+                        .nss .row {
+                            margin-right: auto;
+                            margin-left: auto
+                        }
+
+                        .nss .row[class*=col] {
+                            margin-left: 0;
+                            margin-right: 0
+                        }
+
+                        .nss .dpdw._hov:hover .tgl:not(:checked)+.trig>svg:last-of-type {
+                            transform: rotate(180deg)
+                        }
+
+                        .nss .cms .tabs>[type=radio]:not(:checked)+label,
+                        .nss .var-w .vi:not(:disabled):not(:checked)+.vl {
+                            cursor: pointer
+                        }
+
+                        .nss .col12 {
+                            flex-basis: 75%;
+                            max-width: 75%;
+                            min-width: 75%;
+                            width: 75%
+                        }
+
+                        .nss .col16 {
+                            flex-basis: 100%;
+                            max-width: 100%;
+                            min-width: 100%;
+                            width: 100%
+                        }
+
+                        .nss .dp:not(:disabled) {
+                            background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath d='M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z'/%3E%3Cpath fill='none' d='M0 0h24v24H0z'/%3E%3C/svg%3E") no-repeat right 0
+                        }
+
+                        @media (max-width:1200px) {
+                            .nss .row {
+                                max-width: 950px
+                            }
+                        }
+
+                    </style>
+                    <div class="col12 row">
+                        <div class="f-t col16 -pbm" style="margin-top: 2rem; margin-left: 1rem;">MODES DE PAIEMENT ET LIVRAISON</div>
+                        <div class="col16 -phn -df -fw-w" style="margin-top: .5rem; margin-left: 1rem;">
+                            <a title="Paiement à la livraison" class="ic-link -pas" style="margin-right: 1rem;">
+                                <img src="{{ asset('uploads/logo/cod.svg') }}" height="35" style="width: auto" alt="">
+                            </a>
+                            <a title="Visa" class="ic-link -pas" style="margin-right: 1rem;">
+                                <img src="{{ asset('uploads/logo/visa.svg') }}" height="40" style="width: auto" alt="">
+                            </a>
+                            <a title="Mastercard" class="ic-link -pas" style="margin-right: 1rem;">
+                                <img src="{{ asset('uploads/logo/mastercard.svg') }}" height="40" style="width: auto" alt="">
+                            </a>
                         </div>
-                        <form action="{{ route('subscribers.store') }}" method="post" role="form">
-                            @csrf
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                <input type="email" required="" class="form-control" id="subscribeEmail" name="email" placeholder="EMAIL ADDRESS">
-                            </div>
-                            <input type="submit" class="form-control" value="s'inscrire" id="submit" name="submit">
-                        </form>
-
                     </div>
+                </div>
+                <div class="col-sm-1 col-xs-2">
+                    <!-- <div class="col-sm-6 m0"> -->
+
+
+                </div>
+                <div class="col-sm-4 widget">
+                    <!-- <div class="col-sm-6 m0"> -->
+                    <h4>Plan du site</h4>
+                    <ul class="nav col-xs-6">
+                        <li><a href="{{url('/')}}">Accueil</a></li>
+                        <li><a href="{{url('aboutus')}}">À propos</a></li>
+                        <li><a href="{{url('all_products')}}">Nos produits</a></li>
+                        <li><a href="{{url('flash_products')}}">Promotions</a></li>
+                    </ul>
+                    <ul class="nav col-xs-6">
+                        <li><a href="{{url('flash_products')}}">Destockage</a></li>
+                        <li><a href="{{url('recruitment')}}">Recrutement</a></li>
+                        <li><a href="{{url('videos')}}">Vidéothèque</a></li>
+                        <li><a href="{{url('contact')}}">Contact</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="col-sm-4 widget">
+                    <div class="text-center mb30">
+                        <h4>Contact</h4>
+                        <p>Adresse: High dis 99-100 rue Metz Casablanca - Maroc</p>
+                        <p>Téléphone: +212 522 450 854</p>
+                        <p>Email: contact@bricolya.com </p>
+                    </div>
+                    <form action="{{ route('subscribers.store') }}" method="post" role="form">
+                        @csrf
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                            <input type="email" required="" class="form-control" id="subscribeEmail" name="email" placeholder="EMAIL ADDRESS">
+                        </div>
+                        <input type="submit" class="form-control" value="s'inscrire" id="submit" name="submit">
+                    </form>
+
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="m0 copyRight text-center">
-          <div class="mb15"><a>Déclaration du cookie</a> | <a href="{{asset('static_files/Politique_de_confidentialite-Bricolya.pdf')}}" target="_blank" download>Politique de confidentialité</a>  | <a href="{{asset('static_files/Conditions_generales_de_vente-Bricolya.pdf')}}" target="_blank" download>Conditions générales</a></div>
-          <div class="">Copyright &#169; <a href="{{url('/')}}">Bricolya</a>  2020 - By Cloudme International</div>
+    <div class="m0 copyRight text-center">
+        <div class="mb15"><a>Déclaration du cookie</a> | <a href="{{asset('static_files/Politique_de_confidentialite-Bricolya.pdf')}}" target="_blank" download>Politique de confidentialité</a> | <a href="{{asset('static_files/Conditions_generales_de_vente-Bricolya.pdf')}}" target="_blank" download>Conditions générales</a></div>
+        <div class="">Copyright &#169; <a href="{{url('/')}}">Bricolya</a> 2020 - By Cloudme International</div>
 
-        </div>
+    </div>
 
-    </footer>
+</footer>
